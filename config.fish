b38a5b11b7654bd5e282c7a509d4b30935d10dd0
Taylor Beeston <beeston.taylor@gmail.com>
2021-03-17 05:44:41 -0700

    :art: 3/16/21 Update

--- config.fish
+++ config.fish
@@ -7,8 +7,12 @@ set -gx BROWSER /usr/bin/google-chrome-stable
 alias gaa="git add -A && git status"
 alias gac="gaa && gitmoji -c"
 alias glo="git log --oneline"
+alias gl="git cherry -v master"
 alias git=hub
 alias emptyswap="sudo swapoff -a && sudo swapon -a"
+alias restartpulse="kill pulseaudio && pulseaudio --start"
+alias speaker="bluetoothctl connect 6C:5A:B5:4E:61:65"
+alias ls=exa
 starship init fish | source
 set -U FZF_LEGACY_KEYBINDINGS 0
 set -gx FZF_DEFAULT_COMMAND 'rg --files --follow --hidden'
@@ -19,3 +23,5 @@ if status --is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell"
     source "$BASE16_SHELL/profile_helper.fish"
 end
+
+thefuck --alias | source
